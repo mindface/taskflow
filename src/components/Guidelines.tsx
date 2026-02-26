13173748cf200a6eb9e0f8e5dabd4c1944ee4590
takanori <asdfghjkl@takanoris-Mac-mini.local>
2026-02-26 15:39:55 +0900

    view home page02

--- src/components/Guidelines.tsx
+++ src/components/Guidelines.tsx
@@ -1,9 +1,8 @@
-const content = await fetch("/md/d.md").then(r => r.text());
+const content = await fetch("/md/homeview.md").then(r => r.text());
 import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
 
 export default function Header() {
-
 
   return (
     <div className="DashBord">
