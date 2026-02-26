9bf6ce198b0231fe6ce58e22aac777091d6349f0
takanori <asdfghjkl@takanoris-Mac-mini.local>
2026-02-26 17:01:50 +0900

    sttings build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -55,6 +55,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   }
 
   tauri::Builder::default()
+    .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_dialog::init())
     .manage(Mutex::new(PreviewState::default()))
--- src/components/Guidelines.tsx
+++ src/components/Guidelines.tsx
@@ -1,14 +1,28 @@
-const content = await fetch("/md/homeview.md").then(r => r.text());
-import remarkGfm from "remark-gfm";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-export default function Header() {
+export default function Guidelines() {
+  const [content, setContent] = useState("")
+  useEffect(() => {
+    (async () => {
+      try {
+        const text = await fetch("/md/homeview.md").then(r => r.text());
+        setContent(text);
+      } catch (e) {
+        setContent("## Error\nMarkdown を読み込めませんでした。");
+        console.error(e);
+      }
+    })();
+  }, []);
 
   return (
     <div className="DashBord">
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-      >{content}</ReactMarkdown>
+      <div className="p-4">
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+        >{content}</ReactMarkdown>
+      </div>
     </div>
   );
 }
